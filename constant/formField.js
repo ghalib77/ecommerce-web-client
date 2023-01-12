58565a971b2fd628a2fc1832e8a7186b04f830a4
ghalib77 <muhamadghalib7@gmail.com>
2023-01-13 00:01:02 +0700

    feat: add form field constanta

---
+++ constant/formField.js
@@ -0,0 +1,33 @@
+export const loginForm = {
+  username: {
+    placeholder: "username",
+    type: "text",
+  },
+  password: {
+    placeholder: "password",
+    type: "password",
+  },
+};
+
+export const registerForm = {
+  name: {
+    placeholder: "name",
+    type: "text",
+  },
+  username: {
+    placeholder: "username",
+    type: "text",
+  },
+  email: {
+    placeholder: "email",
+    type: "email",
+  },
+  password: {
+    placeholder: "password",
+    type: "password",
+  },
+  confirm_password: {
+    placeholder: "confirm_password",
+    type: "password",
+  },
+};
