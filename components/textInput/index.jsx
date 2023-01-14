67dcec09aa9edb3efbbad5ec3b868170ad06519b
ghalib77 <muhamadghalib7@gmail.com>
2023-01-14 12:03:05 +0700

    feat: add textInput component

--- components/textInput/index.jsx
+++ components/textInput/index.jsx
@@ -1,25 +1,30 @@
-export default function TextInput({
-  name,
-  type,
-  placeholder,
-  handleOnChange,
-  error,
-}) {
-  return (
-    <div className="items-start form-control">
-      <label className="label">
-        <span className="label-text">{name}</span>
-      </label>
-      <input
-        type={type}
-        placeholder={placeholder}
-        name={name}
-        className="w-full input input-bordered"
-        onChange={handleOnChange}
-      />
-      <span className="px-1 mt-1 text-xs text-error">
-        {error[name] && null}
-      </span>
-    </div>
-  );
-}
+import classNames from "classnames";
+import { forwardRef } from "react";
+
+const TextInput = forwardRef(
+  ({ name, type, placeholder, handleOnChange, error }, ref) => {
+    return (
+      <div className="items-start form-control">
+        <label className="label">
+          <span className="label-text">{name}</span>
+        </label>
+        <input
+          type={type}
+          placeholder={placeholder}
+          name={name}
+          className={classNames(
+            "w-full input input-bordered",
+            Boolean(error[name]) ? "input-error" : null
+          )}
+          onChange={handleOnChange}
+          ref={ref}
+        />
+        <span className="px-1 mt-1 text-xs text-error">{error[name]}</span>
+      </div>
+    );
+  }
+);
+
+TextInput.displayName = "TextInput";
+
+export default TextInput;
