1080d8bb198839c1ae122f7f61654c2f652dd6f5
ghalib77 <muhamadghalib7@gmail.com>
2023-01-14 12:42:41 +0700

    feat: update initialState for logined user

--- hooks/atom/reducers/loginUser.js
+++ hooks/atom/reducers/loginUser.js
@@ -1,11 +1,20 @@
-const initialStateLoginUser = [];
+const initialStateLoginUser = {
+  id: 0,
+  name: "",
+  username: "",
+  email: "",
+  gender: null,
+  photo_profile: null,
+  email_verified_at: "",
+  created_at: "",
+};
 
 const loginUserReducers = (state = initialStateLoginUser, action) => {
   switch (action.type) {
     case "load_login_user":
       return action.payload;
     case "update_login_user":
-      return action.payload;
+      return { ...state, ...action.payload };
   }
 };
 
