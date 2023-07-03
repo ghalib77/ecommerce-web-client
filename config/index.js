4d19f1f5876d11587f22d06a1a06b629400579bb
ghalib77 <muhamadghalib7@gmail.com>
2023-07-03 13:49:14 +0700

    initial

--- config/index.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
-};
-
-export default config;
--- hooks/atom/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { loginUserReducers, initialStateLoginUser } from "./loginUser";
-import { atomWithReducer } from "jotai/utils";
-
-export const loginUserAtom = atomWithReducer(
-  initialStateLoginUser,
-  loginUserReducers
-);
